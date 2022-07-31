749529d12805c5162aa2fa29d2f453458cc70338
Manu Diaz <mdiaz.sanguino@gmail.com>
2022-07-31 11:15:55 +0200

    Created basics grid classes

---
+++ Assets/Scripts/DomainLayer/Grid/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿namespace DomainLayer.Grid
+{
+    public class Coordinates
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Coordinates(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
