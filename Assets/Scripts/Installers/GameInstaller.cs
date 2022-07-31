6ee2439a39a22b4192fc00ea8f44277cb0b71723
Manu Diaz <mdiaz.sanguino@gmail.com>
2022-07-31 17:15:37 +0200

    Added installers + Updated scenes

---
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,30 @@
+﻿using InterfaceAdaptersLayer;
+using UnityEngine;
+
+namespace Installers
+{
+    public class GameInstaller : MonoBehaviour
+    {
+        private SplashScreenInstaller _splashInstaller;
+        private IEventDispatcher _eventDispatcher;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            
+            // TODO: Initialize Ads, Analytics, etc
+            InitializeDependencies();
+        }
+
+        private void InitializeDependencies()
+        {
+            _eventDispatcher = new EventDispatcher();
+            _splashInstaller = new SplashScreenInstaller(_eventDispatcher);
+        }
+
+        private void Start()
+        {
+            _splashInstaller.InitializeGame();
+        }
+    }
+}
